53a41c41d5689db949a1cc0ef1a833466b909030
Jimy Duong <jimyduong1@gmail.com>
2025-01-07 00:39:19 +0700

    create UI SM

--- app/javascript/pages/Entry/index.jsx
+++ app/javascript/pages/Entry/index.jsx
@@ -20,118 +20,139 @@ export default function () {
     <>
       <Header />
       <main>
-        <div className="bg-green2 pt-5 pb-5 w-100 d-flex justify-content-between">
+        <div className="bg-green2 pt-5 pb-5 w-100 d-flex justify-content-between p-12-0-sm">
           <div className="container">
-            <div className="d-flex gap-70 align-items-center">
+            <div className="d-flex gap-70 align-items-center entry-header gap-24-sm">
               <div
-                className="bg-white  text-green3 width-160 height-60 fw-bold fz-26 text-center border-radius-10 align-content-center cursor-pointer">キャンセル
+                className="bg-white text-green3 width-160 height-60 fw-bold fz-26 text-center border-radius-10
+                align-content-center cursor-pointer w-100-sm fz-16-sm bg-green3-sm c-white-sm border-radius-06-sm height-52-sm">キャンセル
               </div>
-              <div className="d-flex gap-40 align-items-center">
-                <img src="/images/result-2.png" alt="" className="width-200 height-200 border-radius-50"/>
+              <div className="d-flex gap-40 align-items-center w-100-sm">
+                <img src="/images/result-2.png" alt="" className="width-200 height-200 border-radius-50 width-80-sm height-80-sm"/>
                 <div className="">
-                  <div className="text-green4 fz-28 fw-bold">第6回朝日市民バドミントン大会</div>
-                  <div className="text-green4 fz-42 fw-bold">エントリー内容の入力</div>
+                  <div className="text-green4 fz-28 fw-bold fz-16-sm">第6回朝日市民バドミントン大会</div>
+                  <div className="text-green4 fz-42 fw-bold fz-24-sm">エントリー内容の入力</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="pt-5 pb-5 w-100">
-          <div className="container">
-            <div className="pd-40 bg-green09 border-radius-10">
-              <div className="fw-bold fz-28 text-green3">チーム名を入力してください</div>
-              <input type="text" className="mt-15 w-100 border-radius-10 height-80 border-0 text-indent-20 fz-24"
+          <div className="container pd-0-sm">
+            <div className="pd-40 bg-green09 border-radius-10 border-radius-0-sm p-12-sm">
+              <div className="fw-bold fz-28 text-green3 fz-18-sm">チーム名を入力してください</div>
+              <input type="text" className="mt-15 w-100 border-radius-10 height-80 border-0 text-indent-20 fz-24 fz-16-sm
+              border-radius-06-sm height-52-sm mt-12-sm"
                      placeholder="チーム名入りました"/>
             </div>
           </div>
-          <div className="container mt-4">
-            <div className="fw-bold fz-28 text-green3">エントリーの種類を選んでください</div>
-            <div className="d-flex gap-10 mt-1">
-              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline">
+          <div className="container mt-4 mt-0-sm p-t-64-sm">
+            <div className="fw-bold fz-28 text-green3 fz-20-sm">エントリーの種類を選んでください</div>
+            <div className="d-flex gap-10 mt-1 flex-direction-column-sm">
+              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline fz-16-sm">
                 <i className="fa-solid fa-circle-check height-20 width-20"></i>
                 <div>男子2部1位</div>
               </div>
-              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline">
+              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline fz-16-sm">
                 <i className="fa-solid fa-circle-check height-20 width-20"></i>
                 <div>男子ダブルス2位</div>
               </div>
-              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline">
+              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline fz-16-sm">
                 <i className="fa-solid fa-circle-check height-20 width-20"></i>
                 <div>女子ダブルス1位</div>
               </div>
-              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline">
+              <div className="d-flex gap-10 height-60 bg-silver6 border-radius-06 pd-20 flex-grow-1 align-items-baseline fz-16-sm">
                 <i className="fa-solid fa-circle-check height-20 width-20"></i>
                 <div>女子ダブルス2位</div>
               </div>
             </div>
           </div>
         </div>
-        <div className="bg-green2 pt-5 pb-5 w-100">
-          <div className="container">
-            <div className="d-flex gap-70">
+        <div className="bg-green2 pt-5 pb-5 w-100 pt-0-sm">
+          <div className="container p-l-r-0-sm">
+            <div className="d-flex gap-70 flex-direction-column-sm gap-0-sm">
               <div className="flex-2-5">
                 <div>
-                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold">男子ダブルス１位</div>
-                  <div className="bg-white pd-25-40">
-                    <div>メンバー：１ペア</div>
-                    <div className="bg-green4 pd-30 mt-15 border-radius-08">
+                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold fz-18-sm pd-22-12-sm">男子ダブルス１位</div>
+                  <div className="bg-white pd-25-40 pd-28-12-sm">
+                    <div className="fz-24 fz-16-sm">メンバー：１ペア</div>
+                    <div className="bg-green4 pd-30 mt-15 border-radius-08 pd-25-sm">
                       <div className="d-flex align-items-center justify-content-between">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6th-oTbkDMbDOPGU_kkRMM55lfvRYgM8JA&s" alt="" className="width-80 height-80 border-radius-50"/>
-                        <div className="text-green3 fw-bold fz-24">Kofworkshard</div>
-                        <select name="" id="" className="height-60 border-0 pd-17-19 border-radius-08">
-                          <option value="1">通常参加費：2,000円</option>
-                        </select>
-                        <div className="fz-18 cursor-pointer">削除</div>
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6th-oTbkDMbDOPGU_kkRMM55lfvRYgM8JA&s"
+                          alt="" className="width-80 height-80 border-radius-50 width-86-sm height-86-sm"/>
+                        <div className="d-flex align-items-center gap-16 gap-03-sm flex-direction-column-sm align-items-flex-start-sm">
+                          <div className="text-green3 fw-bold fz-24 fz-20-sm">Kofworkshard</div>
+                          <select name="" id="" className="height-60 border-0 pd-17-19 border-radius-08 height-38-sm border-radius-06-sm pd-0-13-sm fz-14-sm">
+                            <option value="1">通常参加費：2,000円</option>
+                          </select>
+                          <div className="fz-18 cursor-pointer fz-14-sm align-self-flex-end-sm">削除</div>
+                        </div>
                       </div>
                       <div className="d-flex align-items-center justify-content-between mt-30">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6th-oTbkDMbDOPGU_kkRMM55lfvRYgM8JA&s" alt="" className="width-80 height-80 border-radius-50"/>
-                        <div className="text-green3 fw-bold fz-24">Kofworkshard</div>
-                        <select name="" id="" className="height-60 border-0 pd-17-19 border-radius-08">
-                          <option value="1">通常参加費：2,000円</option>
-                        </select>
-                        <div className="fz-18 cursor-pointer">削除</div>
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6th-oTbkDMbDOPGU_kkRMM55lfvRYgM8JA&s"
+                          alt="" className="width-80 height-80 border-radius-50 width-86-sm height-86-sm"/>
+                        <div className="d-flex align-items-center gap-16 gap-03-sm flex-direction-column-sm align-items-flex-start-sm">
+                          <div className="text-green3 fw-bold fz-24 fz-20-sm">Kofworkshard</div>
+                          <select name="" id="" className="height-60 border-0 pd-17-19 border-radius-08 height-38-sm border-radius-06-sm pd-0-13-sm fz-14-sm">
+                            <option value="1">通常参加費：2,000円</option>
+                          </select>
+                          <div className="fz-18 cursor-pointer fz-14-sm align-self-flex-end-sm">削除</div>
+                        </div>
                       </div>
                     </div>
-                    <div className="w-100 height-60 bg-silver7 border-radius-08 align-content-center text-center cursor-pointer mt-20">ダブルスの参加者を追加</div>
+                    <div className="w-100 height-60 bg-silver7 border-radius-08
+                    align-content-center text-center cursor-pointer mt-20 fz-14-sm pd-22-12-sm">ダブルスの参加者を追加
+                    </div>
                   </div>
                 </div>
-                <div className="mt-20">
-                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold">エントリー代表者情報</div>
-                  <div className="bg-white pd-25-40">
-                    <div className="fz-24">郵便番号</div>
-                    <input type="text" placeholder="郵便番号へ。" className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0"/>
-                    <div className="fz-24 mt-20">アドレス</div>
-                    <input type="text" placeholder="アドレスへ。" className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0"/>
-                    <div className="fz-24 mt-20">電話番号</div>
-                    <input type="text" placeholder="電話番号へ。" className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0"/>
+                <div className="mt-20 mt-10-sm">
+                  <div
+                    className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold pd-22-12-sm">エントリー代表者情報
+                  </div>
+                  <div className="bg-white pd-25-40 pd-22-12-sm">
+                    <div className="fz-24 fz-16-sm">郵便番号</div>
+                    <input type="text" placeholder="郵便番号へ。"
+                           className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0 fz-15-sm height-53-sm border-radius-06-sm"/>
+                    <div className="fz-24 mt-20 fz-16-sm">アドレス</div>
+                    <input type="text" placeholder="アドレスへ。" className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0 fz-15-sm height-53-sm border-radius-06-sm"/>
+                    <div className="fz-24 mt-20 fz-16-sm">電話番号</div>
+                    <input type="text" placeholder="電話番号へ。" className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0 fz-15-sm height-53-sm border-radius-06-sm"/>
                   </div>
                 </div>
-                <div className="mt-20">
-                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold">備考</div>
+                <div className="mt-20 mt-10-sm">
+                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold pd-22-12-sm">備考</div>
                   <div className="bg-white height-200"></div>
                 </div>
               </div>
-              <div className="flex-1">
+              <div className="flex-1 mt-10-sm">
                 <div>
                   <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold">エントリー金額</div>
-                  <div className="pd-25-40 bg-white">
-                    <div className="fz-24">男子ダブルス</div>
-                    <div className="fz-18 mt-30">通常参加費：2,000円×2名</div>
-                    <div className="fz-18">小計4,000円</div>
-                    <div className="text-right fz-24 fw-bold mt-57 ">合計：4,000円</div>
+                  <div className="pd-25-40 bg-white pd-22-12-sm">
+                    <div className="fz-24 fz-18-sm">男子ダブルス</div>
+                    <div className="fz-18 mt-30 fz-14-sm">通常参加費：2,000円×2名</div>
+                    <div className="fz-18 fz-14-sm">小計4,000円</div>
+                    <div className="text-right fz-24 fw-bold mt-57 fz-17-sm">合計：4,000円</div>
                   </div>
-                  <div className="text-center fz-16 bg-silver7 height-60 align-content-center cursor-pointer">保存</div>
+                  <div className="pd-0-12-sm pb-22-sm bg-white-sm">
+                    <div
+                      className="text-center fz-16 bg-silver7 height-60 align-content-center cursor-pointer fz-14-sm border-radius-06-sm">保存
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div className="d-flex justify-content-between mt-113">
+            <div className="d-flex justify-content-between mt-113 mt-0-sm pd-64-12-24-sm">
               <div
-                className="bg-white text-green3 width-180 height-70 fw-bold fz-26 text-center border-radius-10 align-content-center cursor-pointer">キャンセル
+                className="bg-white text-green3 width-180 height-70 fw-bold fz-26 text-center border-radius-10
+                 align-content-center cursor-pointer fz-16-sm height-52-sm width-111-sm border-radius-06-sm">キャンセル
               </div>
               <a href={`/tournament/${params.id}/entry/payment`}>
                 <div
-                  className="text-white bg-green3 width-300 height-70 fw-bold fz-26 text-center border-radius-10 align-content-center cursor-pointer">エントリーに進む
+                  className="text-white bg-green3 width-300 height-70 fw-bold fz-26 text-center
+                   border-radius-10 align-content-center cursor-pointer fz-16-sm height-52-sm width-220-sm border-radius-06-sm">エントリーに進む
                 </div>
               </a>
             </div>
--- app/javascript/pages/Entry/payment.jsx
+++ app/javascript/pages/Entry/payment.jsx
@@ -48,62 +48,66 @@ export default function () {
     <>
       <Header />
       <main>
-        <div className="bg-green2 pt-5 pb-5 w-100 d-flex justify-content-between">
+        <div className="bg-green2 pt-5 pb-5 w-100 d-flex justify-content-between p-12-0-sm">
           <div className="container">
-            <div className="d-flex gap-70 align-items-center">
+            <div className="d-flex gap-70 align-items-center entry-header gap-24-sm">
               <div
-                className="bg-white  text-green3 width-160 height-60 fw-bold fz-26 text-center border-radius-10 align-content-center cursor-pointer">キャンセル
+                className="bg-white text-green3 width-160 height-60 fw-bold fz-26 text-center border-radius-10
+                align-content-center cursor-pointer w-100-sm fz-16-sm bg-green3-sm c-white-sm border-radius-06-sm height-52-sm">入力画面へ
               </div>
-              <div className="d-flex gap-40 align-items-center">
-                <img src="/images/result-2.png" alt="" className="width-200 height-200 border-radius-50"/>
+              <div className="d-flex gap-40 align-items-center w-100-sm">
+                <img src="/images/result-2.png" alt=""
+                     className="width-200 height-200 border-radius-50 width-80-sm height-80-sm"/>
                 <div className="">
-                  <div className="text-green4 fz-28 fw-bold">第6回朝日市民バドミントン大会</div>
-                  <div className="text-green4 fz-42 fw-bold">エントリー内容の入力</div>
+                  <div className="text-green4 fz-28 fw-bold fz-16-sm">第6回朝日市民バドミントン大会</div>
+                  <div className="text-green4 fz-42 fw-bold fz-24-sm">エントリー内容の確認</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="pt-5 pb-5 w-100">
-          <div className="container">
+        <div className="pt-5 pb-5 w-100 pd-0-sm">
+          <div className="container pd-40-0-sm">
             <div>
               <div
-                className="bg-green2 height-76 fz-28 fw-bold text-center b-t-r-right-left-8 align-content-center">この内容でエントリーを行います。よろしいですか？
+                className="bg-green2 height-76 fz-28 fw-bold text-center b-t-r-right-left-8
+                align-content-center fz-18-sm pd-22-12-sm border-radius-0-sm text-left-sm height-92-sm">この内容でエントリーを行います。よろしいですか？
               </div>
-              <div className="pd-40 bg-green4 b-b-r-right-left-8">
-                <div className="height-80 border-radius-10 text-center bg-white align-content-center">
-                  <span className="fw-bold fz-24">お支払い金額：</span>
-                  <span className="fw-bold fz-32 c-red-1">4,000円</span>
+              <div className="pd-40 bg-green4 b-b-r-right-left-8 pd-22-12-sm border-radius-0-sm">
+                <div className="height-80 border-radius-10 text-center bg-white align-content-center height-53-sm border-radius-06-sm">
+                  <span className="fw-bold fz-24 fz-14-sm">お支払い金額：</span>
+                  <span className="fw-bold fz-32 c-red-1 fz-24-sm">4,000円</span>
                 </div>
 
                 <div
-                  className="p-d-l-41 p-d-r-41 height-80 border-radius-06 align-content-center mt-83 p-d-r-41 fz-24 fw-bold bg-white">男子ダブルス2位
+                  className="p-d-l-41 p-d-r-41 height-80 border-radius-06 align-content-center
+                  mt-83 fz-24 fw-bold bg-white fz-14-sm height-53-sm p-d-r-22-sm p-d-l-22-sm mt-40-sm">男子ダブルス2位
                 </div>
-                <div className="p-d-l-41 p-d-r-41 fz-24 m-t-25 c-grey-1">通常参加費：2,000 × 2名 = 4,000円</div>
-                <div className="mg-0-41 division"></div>
-                <div className="p-d-l-41 p-d-r-41">
-                  <span className="fz-24 text-grey1">種目小計：</span>
-                  <span className="fz-32 c-red-1 c-red-1 fw-bold">4,000円</span>
+                <div className="p-d-l-41 p-d-r-41 fz-24 m-t-25 c-grey-1 p-d-r-22-sm p-d-l-22-sm fz-14-sm">通常参加費：2,000 × 2名 = 4,000円</div>
+                <div className="mg-0-41 division mg-0-22-sm"></div>
+                <div className="p-d-l-41 p-d-r-41 p-d-r-22-sm p-d-l-22-sm">
+                  <span className="fz-24 c-grey-1 fz-14-sm">種目小計：</span>
+                  <span className="fz-32 c-red-1 c-red-1 fw-bold fz-18-sm">4,000円</span>
                 </div>
-                <div className="mt-55"></div>
-                <div className="fw-bold fz-24">参加者一覧</div>
-                <div className="mt-16 bg-white border-radius-08 d-flex border-gray-1">
-                  <div className="d-flex align-items-center gap-23 border-r-1 pd-43 flex-1">
+                <div className="mt-55 mt-30-sm"></div>
+                <div className="fw-bold fz-24 fz-14-sm">参加者一覧</div>
+                <div className="mt-16 bg-white border-radius-08 d-flex flex-direction-column-sm">
+                  <div className="d-flex align-items-center gap-23 border-r-1 pd-43 flex-1 border-r-0-sm border-b-1-sm pd-25-sm">
                     <img
                       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6th-oTbkDMbDOPGU_kkRMM55lfvRYgM8JA&s"
-                      alt="" className="width-114 height-114 border-radius-50"/>
+                      alt="" className="width-114 height-114 border-radius-50 width-86-sm height-86-sm"/>
                     <div>
-                      <div className="fw-bold fz-24 text-green3">Kofworkshard</div>
-                      <div className="fz-20 text-green3">通常参加費：2,000円</div>
+                      <div className="fw-bold fz-24 text-green3 fz-20-sm">Kofworkshard</div>
+                      <div className="fz-20 text-green3 fz-16-sm">通常参加費：2,000円</div>
                     </div>
                   </div>
-                  <div className="d-flex align-items-center gap-23 pd-43 flex-1">
+                  <div className="d-flex align-items-center gap-23 pd-43 flex-1  pd-25-sm">
                     <img
                       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6th-oTbkDMbDOPGU_kkRMM55lfvRYgM8JA&s"
-                      alt="" className="width-114 height-114 border-radius-50"/>
+                      alt="" className="width-114 height-114 border-radius-50 width-86-sm height-86-sm"/>
                     <div>
-                      <div className="fw-bold fz-24 text-green3">Kofworkshard</div>
-                      <div className="fz-20 text-green3">通常参加費：2,000円</div>
+                      <div className="fw-bold fz-24 text-green3 fz-20-sm">Kofworkshard</div>
+                      <div className="fz-20 text-green3 fz-16-sm">通常参加費：2,000円</div>
                     </div>
                   </div>
                 </div>
@@ -113,16 +117,15 @@ export default function () {
 
             <div className="m-t-20">
               <div
-                className="bg-green2 height-76 fz-28 fw-bold text-center b-t-r-right-left-8 align-content-center">参加費のお支払い方法について
+                className="bg-green2 height-76 fz-28 fw-bold text-center
+                b-t-r-right-left-8 align-content-center fz-18-sm height-68-sm pd-22-12-sm border-radius-0-sm text-left-sm">参加費のお支払い方法について
               </div>
-              <div className="pd-40 bg-green4 b-b-r-right-left-8">
+              <div className="pd-40 bg-green4 b-b-r-right-left-8 pd-22-12-sm border-radius-0-sm">
                 <div
-                  className="text-center c-grey-1 fz-24">当日受付の際にお支払いください。お釣りのないようにご協力お願いいたします。
+                  className="text-center c-grey-1 fz-24 fz-14-sm text-left-sm">当日受付の際にお支払いください。お釣りのないようにご協力お願いいたします。
                 </div>
 
                 <div className="lead">
-                  クレジットカード情報をご入力のうえ、「決済する」ボタンをクリックしてください。<br/>
-                  ※ VISA / MasterCard のみご利用いただけます。<br/>
                   <p className="logo">
                     <i className="fa-brands fa-cc-amex"></i>
                     <i className="fa-brands fa-cc-visa"></i>
@@ -131,16 +134,18 @@ export default function () {
                   </p>
                 </div>
                 <div className="mt-20">
-                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold">クレジットカード情報</div>
-                  <div className="bg-white pd-25-40">
-                    <div className="fz-24">カード番号</div>
+                  <div className="fz-24 bg-green3 height-76 text-white pd-25-40 fw-bold fz-16-sm pd-22-12-sm">クレジットカード情報</div>
+                  <div className="bg-white pd-25-40 pd-22-12-sm">
+                    <div className="fz-24 fz-16-sm">カード番号</div>
                     <input type="text" placeholder="カード番号"
-                           className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-100 border-0"
+                           className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3
+                            bg-green4 m-t-10 w-100 border-0 fz-16-sm height-53-sm border-radius-06-sm"
                            onChange={(e) => handleChangeCardNumber(e)}
                     />
-                    <div className="fz-24 mt-20">カード有効期限</div>
+                    <div className="fz-24 mt-20 fz-16-sm">カード有効期限</div>
                     <select onChange={(e) => handleChangeCardMonth(e)}
-                            className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-30 border-0">
+                            className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3
+                             bg-green4 m-t-10 w-30 border-0 fz-16-sm height-53-sm border-radius-06-sm w-40-sm">
                       <option value="">--</option>
                       <option value="1">1</option>
                       <option value="2">2</option>
@@ -156,7 +161,8 @@ export default function () {
                       <option value="12">12</option>
                     </select><i></i><span>月 /</span>
                     <select onChange={(e) => handleChangeCardYear(e)}
-                            className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-30 border-0">
+                            className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4
+                             m-t-10 w-30 border-0 fz-16-sm height-53-sm border-radius-06-sm w-40-sm">
                       <option value="">--</option>
                       <option value="2025">2025</option>
                       <option value="2026">2026</option>
@@ -170,16 +176,17 @@ export default function () {
                       <option value="2034">2034</option>
                       <option value="2035">2035</option>
                     </select><i></i><span>年</span>
-                    <div className="fz-24 mt-20">セキュリティコード</div>
+                    <div className="fz-24 mt-20 fz-16-sm">セキュリティコード</div>
                     <input type="text" placeholder="セキュリティコード"
-                           className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3 bg-green4 m-t-10 w-30 border-0"
+                           className="fz-24 height-80 text-indent-20 border-radius-10 bg-green3
+                           bg-green4 m-t-10 w-30 border-0 fz-16-sm height-53-sm border-radius-06-sm w-100-sm"
                            onChange={(e) => handleChangeCardCvv(e)}
                     />
                   </div>
                 </div>
                 <div className="text-center c-grey-1 fz-24 m-t-60">
-                  <div className="d-flex justify-content-center">
-                    <label className="checkbox-style2">
+                  <div className="d-flex justify-content-center justify-content-start-sm">
+                    <label className="checkbox-style2 fz-14-sm">
                       <input type="checkbox" className="d-none"/>
                       <span className="checkmark"></span>
                       <div>上記内容（参加費の支払い方法）を確認しました。</div>
@@ -188,9 +195,9 @@ export default function () {
                   </div>
                   <div className="m-t-40 d-flex justify-content-center gap-10">
                     <button
-                      className="bg-white text-green5 height-57 border-radius-06 fw-bold fz-20 border-0 width-198">キャンセル
+                      className="bg-white text-green5 height-57 border-radius-06 fw-bold fz-20 border-0 width-198 height-52-sm fz-16-sm">キャンセル
                     </button>
-                    <button className="bg-green3 height-57 border-radius-06 fw-bold fz-20 border-0 text-white width-360"
+                    <button className="bg-green3 height-57 border-radius-06 fw-bold fz-20 border-0 text-white width-360  height-52-sm fz-16-sm"
                             disabled={cardNumber === '' || cardMonth === '' || cardYear === '' || cardCvv === ''}
                             onClick={handleSubmit}
                     >進み
