e21a798f0ac697bb5d968820c36c706465cb11ca
Jimy Duong <jimyduong1@gmail.com>
2025-01-17 15:29:59 +0700

    remove comment

--- db/migrate/20241029210435_create_tournament_tables.rb
+++ db/migrate/20241029210435_create_tournament_tables.rb
@@ -2,7 +2,7 @@ class CreateTournamentTables < ActiveRecord::Migration[7.0]
   def change
     create_table :tournament_tables do |t|
       t.integer :table_type, null: false
-      # t.references :match_classes, foreign_key: true, null: false
+      t.references :match_classes, foreign_key: true, null: false
       t.integer :size, null: false
       
       t.timestamps
