f243b028f795435fb427d4fbc9efdc6831dd6e0a
newit-quandd <jimyduong1@gmail.com>
2025-01-25 11:17:53 +0700

    Fix migration file

--- db/migrate/20250124224115_add_change_to_tournament.rb
+++ db/migrate/20250124224115_add_change_to_tournament.rb
@@ -4,6 +4,6 @@ class AddChangeToTournament < ActiveRecord::Migration[7.0]
     add_column :tournaments, :costume_detail, :string
     add_column :tournaments, :transport_information, :string
     add_column :tournaments, :note_for_participants, :text
-    add_column :tournament, :banner, :string
+    add_column :tournaments, :banner, :string
   end
 end
